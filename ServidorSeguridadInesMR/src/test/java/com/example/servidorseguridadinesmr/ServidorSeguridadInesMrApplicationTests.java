6a4e9a47a43fa1dec96ca1c75af5bfd311c68fc8
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-03-20 10:54:40 +0100

    Proyecto IntelliJ creado

---
+++ ServidorSeguridadInesMR/src/test/java/com/example/servidorseguridadinesmr/ServidorSeguridadInesMrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidorseguridadinesmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorSeguridadInesMrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
